224b31288e6038216aefb39c6c8fca84f7f6f661
zacjact1568 <511470661@qq.com>
2023-11-01 22:12:46 +0800

    🚀 更新 AGP & 依赖项

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,11 +73,18 @@ android {
     }
 
     buildFeatures {
+        // 编译时生成 BuildConfig.java
+        buildConfig = true
         viewBinding = true
     }
 }
 
 dependencies {
+    val lifecycle = "2.6.2"
+    val okhttp = "3.14.9"
+    val retrofit = "2.9.0"
+    val stetho = "1.5.0"
+    val room = "2.6.0"
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.10")
@@ -85,24 +92,24 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
     implementation("androidx.fragment:fragment-ktx:1.6.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
-    implementation("androidx.recyclerview:recyclerview:1.3.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("com.airbnb.android:lottie:6.1.0")
     implementation("androidx.datastore:datastore-preferences:1.0.0")
     implementation("com.google.code.gson:gson:2.9.0")
-    implementation("com.squareup.okhttp3:okhttp:3.14.9")
-    implementation("com.squareup.okhttp3:logging-interceptor:3.14.9")
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.facebook.stetho:stetho:1.5.0")
-    implementation("com.facebook.stetho:stetho-okhttp3:1.5.0")
-    implementation("androidx.room:room-runtime:2.5.2")
-    implementation("androidx.room:room-ktx:2.5.2")
+    implementation("com.squareup.okhttp3:okhttp:$okhttp")
+    implementation("com.squareup.okhttp3:logging-interceptor:$okhttp")
+    implementation("com.squareup.retrofit2:retrofit:$retrofit")
+    implementation("com.squareup.retrofit2:converter-gson:$retrofit")
+    implementation("com.facebook.stetho:stetho:$stetho")
+    implementation("com.facebook.stetho:stetho-okhttp3:$stetho")
+    implementation("androidx.room:room-runtime:$room")
+    implementation("androidx.room:room-ktx:$room")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    ksp("androidx.room:room-compiler:2.5.2")
+    ksp("androidx.room:room-compiler:$room")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
-    id("com.android.application") version "8.1.1" apply false
+    id("com.android.application") version "8.1.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+    // KSP 版本，需根据 Kotlin 版本指定
+    // https://github.com/google/ksp/releases
     id("com.google.devtools.ksp") version "1.9.10-1.0.13" apply false
 }
